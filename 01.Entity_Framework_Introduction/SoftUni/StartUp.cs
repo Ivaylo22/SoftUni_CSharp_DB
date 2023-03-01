77588a6d89cfdb0577d37425f64461484289f567
Ivaylo22 <ivi_petkow@abv.bg>
2023-03-01 23:28:30 +0200

    Finished Entity Framework Introduction

--- 01.Entity_Framework_Introduction/SoftUni/StartUp.cs
+++ 01.Entity_Framework_Introduction/SoftUni/StartUp.cs
@@ -12,7 +12,7 @@ public class StartUp
     {
         SoftUniContext dbContext = new SoftUniContext();
 
-        string result = GetEmployeesByFirstNameStartingWithSa(dbContext);
+        string result = RemoveTown(dbContext);
 
         Console.WriteLine(result);
     }
@@ -366,5 +366,36 @@ public class StartUp
             .Select(p => p.Name)
             .ToArray();
         return String.Join(Environment.NewLine, projectNames);
+    }
+
+    //Problem 15:
+    public static string RemoveTown(SoftUniContext context)
+    {
+        var townToDelete = context.Towns
+                .First(t => t.Name == "Seattle");
+
+        IQueryable<Address> addressesToDelete = context.Addresses
+                    .Where(a => a.TownId == townToDelete.TownId);
+
+        int addressesCount = addressesToDelete.Count();
+
+        IQueryable<Employee> employeesOnDeletedAddresses = context.Employees
+                    .Where(e => addressesToDelete.Any(a => a.AddressId == e.AddressId));
+
+        foreach (var employee in employeesOnDeletedAddresses)
+        {
+            employee.AddressId = null;
+        }
+
+        foreach (var address in addressesToDelete)
+        {
+            context.Addresses.Remove(address);
+        }
+
+        context.Remove(townToDelete);
+
+        context.SaveChanges();
+
+        return $"{addressesCount} addresses in Seattle were deleted";
     }
 }
