842b2855a2c1cc0e24e32d0841cdd6b46f8ecd11
Ivaylo22 <ivi_petkow@abv.bg>
2023-03-01 17:48:46 +0200

    Added 04. Employees with Salary Over 50 000

--- 01.Entity_Framework_Introduction/SoftUni/StartUp.cs
+++ 01.Entity_Framework_Introduction/SoftUni/StartUp.cs
@@ -9,7 +9,7 @@ public class StartUp
     {
         SoftUniContext dbContext = new SoftUniContext();
 
-        string result = GetEmployeesFullInformation(dbContext);
+        string result = GetEmployeesWithSalaryOver50000(dbContext);
 
         Console.WriteLine(result);
     }
@@ -35,6 +35,30 @@ public class StartUp
         foreach (var employee in employees)
         {
             sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+
+    //Problem 04:
+    public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var employees = context.Employees
+            .Select(e => new
+            {
+                e.FirstName,
+                e.Salary
+            })
+            .Where(e => e.Salary > 50000)
+            .OrderBy(e => e.FirstName)
+            .ToArray();
+
+        foreach (var employee in employees)
+        {
+            sb
+                .AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
         }
 
         return sb.ToString().TrimEnd();
