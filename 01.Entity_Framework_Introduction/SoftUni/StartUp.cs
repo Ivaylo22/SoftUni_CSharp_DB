46eff19a6f6c668a26766457770fde042da39e5d
Ivaylo22 <ivi_petkow@abv.bg>
2023-03-01 17:40:50 +0200

    Added 03. Employees Full Information

--- 01.Entity_Framework_Introduction/SoftUni/StartUp.cs
+++ 01.Entity_Framework_Introduction/SoftUni/StartUp.cs
@@ -1,4 +1,5 @@
 ﻿using SoftUni.Data;
+using System.Text;
 
 namespace SoftUni;
 
@@ -7,6 +8,35 @@ public class StartUp
     static void Main(string[] args)
     {
         SoftUniContext dbContext = new SoftUniContext();
-        Console.WriteLine("Connected");
+
+        string result = GetEmployeesFullInformation(dbContext);
+
+        Console.WriteLine(result);
+    }
+
+
+    //Problem 03:
+    public static string GetEmployeesFullInformation(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var employees = context.Employees
+            .OrderBy(e => e.EmployeeId)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.MiddleName,
+                e.LastName,
+                e.JobTitle,
+                e.Salary
+            })
+            .ToArray();
+
+        foreach (var employee in employees)
+        {
+            sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+        }
+
+        return sb.ToString().TrimEnd();
     }
 }
