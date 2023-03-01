a7e050821c2774db49cff265950e143582563010
Ivaylo22 <ivi_petkow@abv.bg>
2023-03-01 18:10:57 +0200

    Added 05. Employees from Research and Development

--- 01.Entity_Framework_Introduction/SoftUni/StartUp.cs
+++ 01.Entity_Framework_Introduction/SoftUni/StartUp.cs
@@ -8,8 +8,7 @@ public class StartUp
     static void Main(string[] args)
     {
         SoftUniContext dbContext = new SoftUniContext();
-
-        string result = GetEmployeesWithSalaryOver50000(dbContext);
+        string result = GetEmployeesFromResearchAndDevelopment(dbContext);
 
         Console.WriteLine(result);
     }
@@ -59,6 +58,33 @@ public class StartUp
         {
             sb
                 .AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+
+    //Problem 05:
+    public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var employeesRnD = context.Employees
+            .Where(e => e.Department.Name == "Research and Development")
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                DepartmentName = e.Department.Name,
+                e.Salary
+            })
+            .OrderBy(e => e.Salary)
+            .ThenByDescending(e => e.FirstName)
+            .ToArray();
+
+        foreach (var e in employeesRnD)
+        {
+            sb
+                .AppendLine($"{e.FirstName} {e.LastName} from {e.DepartmentName} - ${e.Salary:f2}");
         }
 
         return sb.ToString().TrimEnd();
