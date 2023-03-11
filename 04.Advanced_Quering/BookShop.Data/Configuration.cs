80292014faf2ff081a347f8b9192fbee98abb7e2
Ivaylo22 <ivi_petkow@abv.bg>
2023-03-11 11:48:57 +0200

    Added Advanced_Quering - 02.Age Restriction

---
+++ 04.Advanced_Quering/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => @"Server=DESKTOP-23ATDCK;Database=BookShop;Integrated Security=True;";
+    }
+}
---
+++ 04.Advanced_Quering/BookShop/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var dbContext = new BookShopContext();
+            //DbInitializer.ResetDatabase(dbContext);
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine(GetBooksByAgeRestriction(dbContext, input));
+            
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+
+            var hasParsed = Enum.TryParse<AgeRestriction>(command, true, out AgeRestriction ageRestrictionObj);
+
+            if(hasParsed)
+            {
+                var books = context.Books
+                    .Where(b => b.AgeRestriction == ageRestrictionObj)
+                    .OrderBy(b => b.Title)
+                    .Select(b => b.Title)
+                    .ToArray();
+
+                return String.Join(Environment.NewLine, books);
+            }
+
+            return null;
+
+        }
+    }
+}
+
+
