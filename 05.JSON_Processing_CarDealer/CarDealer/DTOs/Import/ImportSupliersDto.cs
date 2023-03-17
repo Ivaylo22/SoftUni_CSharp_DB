c89614108c9b70834576e6f75c4d415c2d625052
Ivaylo22 <ivi_petkow@abv.bg>
2023-03-17 10:12:22 +0200

    Started CarDealer
    implemented importSuppliers

---
+++ 05.JSON_Processing_CarDealer/CarDealer/DTOs/Import/ImportSupliersDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CarDealer.DTOs.Import;
+
+public class ImportSupliersDto
+{
+    public string Name { get; set; } = null!;
+
+    public bool IsImporter { get; set; }
+}
---
+++ 05.JSON_Processing_CarDealer/CarDealer/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace CarDealer;
+
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        CarDealerContext dbContext = new CarDealerContext();
+        string inputJson = File.ReadAllText(@"../../../Datasets/suppliers.json");
+
+        string result = ImportSuppliers(dbContext, inputJson);
+        Console.WriteLine(result);
+    }
+
+    public static string ImportSuppliers(CarDealerContext context, string inputJson)
+    {
+        IMapper mapper = CreateMapper();
+
+        Supplier[] supplierDtos = JsonConvert.DeserializeObject<Supplier[]>(inputJson);
+
+        Supplier[] validSuppliers = mapper.Map<Supplier[]>(supplierDtos);
+
+        context.Suppliers.AddRange(validSuppliers);
+        context.SaveChanges();
+
+        return $"Successfully imported {validSuppliers.Length}.";
+    }
+
+    private static IMapper CreateMapper()
+    {
+        return new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CarDealerProfile>();
+        }));
+    }
+
+}
+
