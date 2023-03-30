d068aba62f36f2456d54405aab76be03bfe35c2a
Ivaylo22 <ivi_petkow@abv.bg>
2023-03-30 14:39:07 +0300

    Finished Xml Import

--- 07.Exam_Prep_Trucks/Trucks/DataProcessor/Deserializer.cs
+++ 07.Exam_Prep_Trucks/Trucks/DataProcessor/Deserializer.cs
@@ -1,8 +1,12 @@
 ﻿namespace Trucks.DataProcessor
 {
     using System.ComponentModel.DataAnnotations;
+    using System.Text;
+    using CarDealer.Utilities;
     using Data;
-
+    using Trucks.Data.Models;
+    using Trucks.Data.Models.Enums;
+    using Trucks.DataProcessor.ImportDto;
 
     public class Deserializer
     {
@@ -14,14 +18,67 @@
         private const string SuccessfullyImportedClient
             = "Successfully imported client - {0} with {1} trucks.";
 
+        private static XmlHelper xmlHelper;
+
         public static string ImportDespatcher(TrucksContext context, string xmlString)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+            xmlHelper = new XmlHelper();
+
+            ImportDespatcherDto[] despatchers =
+                xmlHelper.Deserialize<ImportDespatcherDto[]>(xmlString, "Despatchers");
+
+            ICollection<Despatcher> validDespatchers = new HashSet<Despatcher>();
+
+            foreach (var despatcher in despatchers)
+            {
+                if(!IsValid(despatcher))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                ICollection<Truck> validTrucks = new HashSet<Truck>();
+                foreach (ImportTruckDto truckDto in despatcher.Trucks)
+                {
+                    if(!IsValid(truckDto))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    Truck truck = new Truck
+                    {
+                        RegistrationNumber = truckDto.RegistrationNumber,
+                        VinNumber = truckDto.VinNumber,
+                        TankCapacity = truckDto.TankCapacity,
+                        CargoCapacity = truckDto.CargoCapacity,
+                        CategoryType = (CategoryType)truckDto.CategoryType,
+                        MakeType = (MakeType)truckDto.MakeType
+                    };
+
+                    validTrucks.Add(truck);
+                }
+                Despatcher validDespatcher = new Despatcher()
+                {
+                    Name = despatcher.Name,
+                    Position = despatcher.Position,
+                    Trucks = validTrucks
+                };
+                validDespatchers.Add(validDespatcher);
+
+                sb.AppendLine(String.Format(SuccessfullyImportedDespatcher, despatcher.Name, validTrucks.Count));
+            }
+
+            context.AddRange(validDespatchers);
+            context.SaveChanges();
+
+            return sb.ToString().Trim();
         }
-        public static string ImportClient(TrucksContext context, string jsonString)
-        {
-            throw new NotImplementedException();
-        }
+        //public static string ImportClient(TrucksContext context, string jsonString)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
         private static bool IsValid(object dto)
         {
---
+++ 07.Exam_Prep_Trucks/Trucks/DataProcessor/ImportDto/ImportDespatcherDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Trucks.DataProcessor.ImportDto;
+
+[XmlType("Despatcher")]
+public class ImportDespatcherDto
+{
+
+    [XmlElement("Name")]
+    [MinLength(2)]
+    [MaxLength(40)]
+    [Required]
+    public string Name { get; set; } = null!;
+
+    [XmlElement("Position")]
+    [Required]
+    public string Position { get; set; } = null!;
+
+    [XmlArray("Trucks")]
+    public ImportTruckDto[] Trucks { get; set; }
+}
